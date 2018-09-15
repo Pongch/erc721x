cdb5d1895f1d5654958978961cb1eb5d2f2d447b
Georgios Konstantopoulos <me@gakonst.com>
2018-09-15 15:30:02 +0300

    Truffle v4 compatibility

--- contracts/Core/ERC721X/ERC721XToken.sol
+++ contracts/Core/ERC721X/ERC721XToken.sol
@@ -144,7 +144,7 @@ contract ERC721XToken is ERC721X, ERC721XTokenNFT {
         internal
         isOperatorOrOwner(_from)
     {
-        require(_amount <= balanceOf(_from, _tokenId), "Quantity greater than from balance");
+        require(_amount <= balanceOfCoin(_from, _tokenId), "Quantity greater than from balance");
         require(_to != address(0), "Invalid to address");
 
         _updateTokenBalance(_from, _tokenId, _amount, ObjectLib.Operations.SUB);
